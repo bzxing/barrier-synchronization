85363dddf40212435fcd538dcae7c4d96ba760b1
Bill Z. Xing <bill.z.xing@gmail.com>
2018-03-12 21:11:49 -0400

    Counter impl 2

--- mpi/gtmpi_counter.cpp
+++ mpi/gtmpi_counter.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <mpi.h>
+#include <vector>
 
 #include "my_utils.h"
 
@@ -31,11 +32,15 @@ extern "C" {
 
 
 static unsigned s_world_size = kUnsignedInvalid;
+static std::vector<MPI_Request> s_reqs;
+
+
 // zxing7: Also save the dynamic allocation of the status array that's completely not used.
 
 void gtmpi_init(int num_threads)
 {
     s_world_size = boost::numeric_cast<unsigned>(num_threads);
+    s_reqs.resize(s_world_size);
 }
 
 void gtmpi_barrier()
@@ -49,27 +54,50 @@ void gtmpi_barrier()
     // Instead every node sending to every other node which is O(n^2),
     // we can make it O(n), as follow:
 
-    // If not first node, wait for previous node to arrive
-    if (rank != 0)
-    {
-        MPI_Recv(nullptr, 0, MPI_UNSIGNED_CHAR, rank - 1, kDefaultTag, MPI_COMM_WORLD, nullptr);
-    }
-    // If not last node, arrive at next node
-    if (rank != s_world_size - 1)
-    {
-        MPI_Send(nullptr, 0, MPI_UNSIGNED_CHAR, rank + 1, kDefaultTag, MPI_COMM_WORLD);
-    }
+    // // If not first node, wait for previous node to arrive
+    // if (rank != 0)
+    // {
+    //     MPI_Recv(nullptr, 0, MPI_UNSIGNED_CHAR, rank - 1, kDefaultTag, MPI_COMM_WORLD, nullptr);
+    // }
+    // // If not last node, arrive at next node
+    // if (rank != s_world_size - 1)
+    // {
+    //     MPI_Send(nullptr, 0, MPI_UNSIGNED_CHAR, rank + 1, kDefaultTag, MPI_COMM_WORLD);
+    // }
 
-    // If not first node, wake up previous node
-    if (rank != 0)
-    {
-        MPI_Send(nullptr, 0, MPI_UNSIGNED_CHAR, rank - 1, kDefaultTag, MPI_COMM_WORLD);
-    }
+    // // If not first node, wake up previous node
+    // if (rank != 0)
+    // {
+    //     MPI_Send(nullptr, 0, MPI_UNSIGNED_CHAR, rank - 1, kDefaultTag, MPI_COMM_WORLD);
+    // }
 
-    // If not last node, wait for being waken up by next node
-    if (rank != s_world_size - 1)
+    // // If not last node, wait for being waken up by next node
+    // if (rank != s_world_size - 1)
+    // {
+    //     MPI_Recv(nullptr, 0, MPI_UNSIGNED_CHAR, rank + 1, kDefaultTag, MPI_COMM_WORLD, nullptr);
+    // }
+
+
+    if (rank == 0)
     {
-        MPI_Recv(nullptr, 0, MPI_UNSIGNED_CHAR, rank + 1, kDefaultTag, MPI_COMM_WORLD, nullptr);
+        for (unsigned i = 1; i < s_world_size; ++i)
+        {
+            int result = MPI_Recv(nullptr, 0, MPI_UNSIGNED_CHAR,
+                boost::numeric_cast<int>(i), kDefaultTag, MPI_COMM_WORLD, nullptr);
+            BOOST_ASSERT(result == MPI_SUCCESS);
+        }
+        // Wake everyone up
+        for (unsigned i = 1; i < s_world_size; ++i)
+        {
+            int result = MPI_Send(nullptr, 0, MPI_UNSIGNED_CHAR,
+                boost::numeric_cast<int>(i), kDefaultTag, MPI_COMM_WORLD);
+            BOOST_ASSERT(result == MPI_SUCCESS);
+        }
+    }
+    else
+    {
+        MPI_Send(nullptr, 0, MPI_UNSIGNED_CHAR, 0, kDefaultTag, MPI_COMM_WORLD);
+        MPI_Recv(nullptr, 0, MPI_UNSIGNED_CHAR, 0, kDefaultTag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
 }
 
